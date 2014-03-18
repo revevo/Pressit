fe3b1f559ea0f6523f2e5bc411fff99c0b63a1f0
Paulo Henrique Lopes Prado <paulo@afold.co>
2014-03-18 10:48:44 -0300

    Space detection improvement

--- scripts/pressit.js
+++ scripts/pressit.js
@@ -2,26 +2,46 @@
 
 	console.log('started');
 
-	function triangleArea(x1, y1, x2, y2, x3, y3) {
-		return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
-
+	function triangleArea(v1, v2, v3) {
+		return Math.abs((v1.x*(v2.y-v3.y) + v2.x*(v3.y-v1.y)+ v3.x*(v1.y-v2.y))/2.0);
 	}
 
-	function isInside(e1, e2, e3, e) {   
-	   /* Calculate area of triangle ABC */
-	    A = triangleArea(e1.x, e1.y, e2.x, e2.y, e3.x, e3.y);
-	 
-	   /* Calculate area of triangle PBC */  
-	   A1 = triangleArea(e.x, e.y, e2.x, e2.y, e3.x, e3.y);
-	 
-	   /* Calculate area of triangle PAC */  
-	   A2 = triangleArea(e1.x, e1.y, e.x, e.y, e3.x, e3.y);
-	 
-	   /* Calculate area of triangle PAB */   
-	   A3 = triangleArea(e1.x, e1.y, e2.x, e2.y, e.x, e.y);
-	   
-	   /* Check if sum of A1, A2 and A3 is same as A */
-	   return A == A1 + A2 + A3;
+	function polygonArea(numPoints, v) { 
+		area = 0;         // Accumulates area in the loop
+		j = numPoints-1;  // The last vertex is the 'previous' one to the first
+
+		for (i=0; i<numPoints; i++) { 
+			area = area + (v[j].x+v[i].x) * (v[j].y-v[i].y); 
+			j = i;  //j is previous vertex to i
+		}
+
+		return Math.abs(area*.5);
+	}
+
+	function isInside(points) {
+
+		arguments  = Array.prototype.slice.call(arguments);
+		if (arguments.length <= 2) {
+			return false;
+		}
+
+		point = arguments.shift();
+		var polygon = polygonArea(arguments.length, arguments);
+		var polygon2 = triangleArea(arguments[0], arguments[1], arguments[2]);
+		var vertices = polygonArea(3, [arguments[0], arguments[arguments.length-1], points]);
+
+		for (i = 0; i < arguments.length - 1; i++) {
+
+			vertices += triangleArea(arguments[i], arguments[i+1], points);
+		}
+
+		console.log(polygon + ' ' + vertices)
+
+		if (vertices == polygon) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	$.fn.pressit = function() {
@@ -36,18 +56,13 @@
 
 			eventXY = {x: event.offsetX, y: event.offsetY};
 
-			istop = isInside(topLeft, topRight, middle, eventXY);
-			isbottom = isInside(bottomRight, bottomLeft, middle, eventXY);
-			isleft = isInside(topLeft, bottomLeft, middle, eventXY);
-			isright = isInside(topRight, bottomRight, middle, eventXY);
-
-			if (istop) {
+			if (isInside(eventXY, topLeft, topRight, middle)) {
 				$(this).addClass('press-top');
-			} else if (isbottom) {
+			} else if (isInside(eventXY, bottomRight, bottomLeft, middle)) {
 				$(this).addClass('press-bottom')
-			} else if (isleft) {
+			} else if (isInside(eventXY, topLeft, bottomLeft, middle)) {
 				$(this).addClass('press-left');
-			} else if (isright) {
+			} else if (isInside(eventXY, topRight, bottomRight, middle)) {
 				$(this).addClass('press-right');
 			}
 
